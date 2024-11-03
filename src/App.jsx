02be0016e3269d36fdbb160195cba0393208509b
Anastasia <anastasiya.reksius@mail.ru>
2024-11-04 00:52:56 +0700

    change class name from import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import MainSection from './MainSection'; 
+import './main.scss';
 
 function App() { 
-  return(
-    <p>Hello world</p>
-  )
-  
+  return (
+    <div>
+      <MainSection /> 
+    </div>
+  );
 }
 
-export default App
+export default App;
+
